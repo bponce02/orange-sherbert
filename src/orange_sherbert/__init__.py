ca593f041542d5f93765a11d0d4394c37f636f02
Bryan Ponce <poncebryan002@gmail.com>
2025-12-03 23:33:11 -0500

    Initial project setup

---
+++ src/orange_sherbert/__init__.py
@@ -0,0 +1,3 @@
+"""Orange Sherbert - A Django CRUD toolkit"""
+
+__version__ = "0.1.0"
---
+++ src/orange_sherbert/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OrangeSherbertConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'orange_sherbert'
+    verbose_name = 'Orange Sherbert'
