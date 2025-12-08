d0df16d3432fb57996ac71533e4ac0632464e137
Bryan Ponce <poncebryan002@gmail.com>
2025-12-08 09:36:18 -0500

    Max width on button group

--- src/example/models.py
+++ src/example/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
-
+    isbn = models.CharField(max_length=13)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    pub_date = models.DateField()
+    checked_out = models.BooleanField(default=False)
     def __str__(self):
         return self.title
 
